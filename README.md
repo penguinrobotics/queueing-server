c0fe2953b3e81194aea4e34edf58595cebde5c86
kevqiu <kevinqiu96@gmail.com>
2024-11-02 22:57:06 -0700

    add amount shift, cleanup

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 1. Install Node.js
 2. Update `SERVER_PATH` in `server.js`
-3. Run `npm i`
+3. Create an empty file called `team_data.json`
+4. Run `npm i`
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const cors = require('cors');
-const Mutex = require('async-mutex').Mutex;
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const cors = require("cors");
+const Mutex = require("async-mutex").Mutex;
 
 const app = express();
 app.use(express.json());
@@ -16,26 +16,26 @@ const server = http.createServer(app);
 /**
  * UPDATE THIS VALUE WITH YOUR VEX TM SERVER IP
  */
-const SERVER_PATH = 'http://10.0.0.2/division1/teams';
+const SERVER_PATH = "http://10.0.0.2/division1/teams";
 
-const dbFile = 'team_data.json';
+const dbFile = "team_data.json";
 let writeLock = new Mutex();
 
 // database lmfao
 let nowServing = [];
 let queue = [];
 
-const wss = new WebSocket.Server({ server, path: '/queue' });
+const wss = new WebSocket.Server({ server, path: "/queue" });
 
 // WebSocket connection handling
-wss.on('connection', (ws) => {
-  console.log('WebSocket connection established');
+wss.on("connection", (ws) => {
+  console.log("WebSocket connection established");
 
   ws.send(JSON.stringify({ nowServing, queue }));
 
   // Handle disconnection
-  ws.on('close', () => {
-    console.log('WebSocket connection closed');
+  ws.on("close", () => {
+    console.log("WebSocket connection closed");
   });
 });
 
@@ -47,7 +47,7 @@ const updateClients = () => {
   });
 };
 
-const fileData = fs.readFileSync(dbFile, 'utf-8');
+const fileData = fs.readFileSync(dbFile, "utf-8");
 if (fileData) {
   const data = JSON.parse(fileData);
   if (data) {
@@ -63,28 +63,28 @@ const updateFile = () => {
     JSON.stringify(
       {
         nowServing,
-        queue
+        queue,
       },
       null,
       2
     ),
-    { flag: 'w' }
+    { flag: "w" }
   );
 };
 
 // Express route handling
-app.get('/', (req, res) => {
-  res.send('Hello, Express Server!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express Server!");
 });
 
-app.post('/add', (req, res) => {
+app.post("/add", (req, res) => {
   writeLock.acquire();
   const { team } = req.body;
   if (
     queue.find((t) => t.number === team) ||
     nowServing.find((t) => t.number === team)
   ) {
-    return res.status(400).json({ error: 'Team is already in queue' });
+    return res.status(400).json({ error: "Team is already in queue" });
   }
   queue.push({ number: team, at: null });
   updateFile();
@@ -93,7 +93,7 @@ app.post('/add', (req, res) => {
   res.status(200).json({ team });
 });
 
-app.post('/serve', (req, res) => {
+app.post("/serve", (req, res) => {
   if (queue.length) {
     writeLock.acquire();
     const next = queue.shift();
@@ -103,28 +103,28 @@ app.post('/serve', (req, res) => {
     writeLock.release();
     res.status(200).json({ team: next });
   } else {
-    res.status(400).json({ error: 'empty' });
+    res.status(400).json({ error: "empty" });
   }
 });
 
-app.post('/unserve', (req, res) => {
-  const { team } = req.body;
+app.post("/unserve", (req, res) => {
+  const { team, amount } = req.body;
   if (nowServing.length) {
     writeLock.acquire();
     const unservedIndex = nowServing.findIndex((t) => t.number === team);
     const unserved = nowServing.find((t) => t.number === team);
     nowServing.splice(unservedIndex, 1);
-    queue.unshift(unserved);
+    queue.splice(amount - 1, 0, unserved);
     updateFile();
     updateClients();
     writeLock.release();
     res.status(200).json({ team: unserved.number });
   } else {
-    res.status(400).json({ error: 'empty' });
+    res.status(400).json({ error: "empty" });
   }
 });
 
-app.post('/remove', (req, res) => {
+app.post("/remove", (req, res) => {
   writeLock.acquire();
   const { team } = req.body;
   nowServing = nowServing.filter((t) => t.number !== team);
@@ -135,21 +135,15 @@ app.post('/remove', (req, res) => {
   res.status(200).json({ team });
 });
 
-app.get('/teams', async (req, res) => {
+app.get("/teams", async (req, res) => {
   const html = await axios.get(SERVER_PATH);
   const $ = cheerio.load(html.data);
 
   const teamData = [];
-  $('tbody tr').each((index, element) => {
-    const $tds = $(element).find('td');
-    const number = $tds
-      .eq(0)
-      .text()
-      .trim();
-    const school = $tds
-      .eq(3)
-      .text()
-      .trim();
+  $("tbody tr").each((index, element) => {
+    const $tds = $(element).find("td");
+    const number = $tds.eq(0).text().trim();
+    const school = $tds.eq(3).text().trim();
 
     teamData.push({ number, school });
   });
